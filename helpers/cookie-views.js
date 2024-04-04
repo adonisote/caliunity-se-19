eb59306c6d684914c1737aa15723bf54104edd42
Adonis <adonis.almagro@code.berlin>
2024-04-04 21:25:48 +0200

    helper function cookies

---
+++ helpers/cookie-views.js
@@ -0,0 +1,4 @@
+export const convertToDollar = (priceInCents) => {
+  return '$' + (priceInCents / 100)
+}
+
--- routes/cookies/index.js
+++ routes/cookies/index.js
@@ -1,15 +1,19 @@
 import express from 'express';
+import { convertToDollar } from '../../helpers/cookie-views.js';
 
 const cookiesRouter = express.Router();
 
 const cookies = [
-  { name: 'Double Choc', slug: 'double-choc', isInStock: true },
-  { name: 'Chocho Chip', slug: 'choco-chip', isInStock: false },
+  { name: 'Double Choc', slug: 'double-choc', priceInCents: 350, isInStock: true },
+  { name: 'Chocho Chip', slug: 'choco-chip', priceInCetns: 350, isInStock: false },
 ];
 const ingredients = ['flour', 'water', 'eggs', 'salt'];
 
 cookiesRouter.get('/', (req, res) => {
-  res.render('cookies/index', { cooki: cookies, ingre: ingredients });
+  res.render('cookies/index', {
+    cooki: cookies, ingre: ingredients,
+    readablePrice: convertToDollar
+  });
 });
 
 export default cookiesRouter;
